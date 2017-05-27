3d61ac93a91ceb88e1ba833980ca3c6c8333b11a
Asia <gibsonjasia@gmail.com>
2017-05-27 18:18:32 -0400

    Testing
    Terrain created, First person view

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+	public Transform Player;
+	int MoveSpeed = 2;
+	int MaxDist = 10;
+	int MinDist = 5;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt(Player);
+
+		if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+		{
+
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+			if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+			{
+
+				//add attack function
+			}
+
+		}
+	}
+	}
+
---
+++ Assets/Scripts/PickUp.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var target : Transform;
+
+function Update () {
+
+}
+
+function OnMouseDown(){
+
+this.transform.position = target.position;
+this.transform.parent = GameObject.Find("FPSController").transform;
+this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+
+}
+
+function OnMouseUP(){
+this.transform.parent = GameObject.Find("FPSController").transform;
+this.transform.parent = null;
+}
